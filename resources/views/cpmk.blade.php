a1bee7f488e125ad87925ee995c3b3eca62b3f78
aidilbaihaqi <aidillmusirjun@gmail.com>
2024-09-02 21:01:53 +0700

    benerin view cpl sama cpmk

--- resources/views/cpmk.blade.php
+++ resources/views/cpmk.blade.php
@@ -72,6 +72,7 @@
                         </td>
                         <td class="px-6 py-4 text-right">
                             <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                            <a href="#" class="font-medium text-red-600 dark:text-red-500 hover:underline">Hapus</a>
                         </td>
                     </tr>
                   @endforeach
