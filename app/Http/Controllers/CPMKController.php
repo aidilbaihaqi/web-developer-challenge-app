6b41fc8cb8a31791f64001d2a77d9d2ccd84cf3a
aidilbaihaqi <aidillmusirjun@gmail.com>
2024-09-02 20:58:04 +0700

    Index and Store: CPMK

---
+++ app/Http/Controllers/CPMKController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Request;
+
+class CPMKController extends Controller
+{
+    public function index() {
+        $response = Http::withHeaders([
+            'Authorization' => 'contohABC'
+        ])->post('http://127.0.0.1:9871/api/listCPMK', [
+            'cpmk' => 'list'
+        ]);
+
+        if($response->successful()) {
+            $data = $response->json();
+            return view('cpmk', [
+                'title' => 'Data CPMK',
+                'data' => $data
+            ]);
+        }
+    }
+    public function store(Request $request) {
+        $request->validate([
+            'kodecpmk' => 'required',
+            'kodecpl' => 'required',
+            'deskripsi' => 'required'
+        ]);
+
+        $response = Http::withHeaders([
+            'Authorization' => 'contohABC'
+        ])->post('http://127.0.0.1:9871/api/addCPMK', [
+            'kodecpmk' => $request->kodecpmk,
+            'kodecpl' => $request->kodecpl,
+            'deskripsi' => $request->deskripsi
+        ]);
+
+        if($response->successful()) {
+            return redirect()->route('cpmk.index')->with(['success' => 'Data berhasil ditambahkan']);
+        }
+    }
+}
